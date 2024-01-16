5607d6993c2466acb36cd21de3bfb5e20932f417
unknown <ujjawal217@gmail.com>
2024-01-16 19:19:42 +0530

    assignments

---
+++ Cohort2.0/Day3(Funct)/in.js
@@ -0,0 +1,12 @@
+const user = {
+    name: 'Ujjawal Indwar',
+    occupation: 'Coder',
+    addresses: {
+        house_no: 11,
+        street: 1
+    }
+}
+
+console.log(user['addresses']['house_no']);
+
+console.log(user.addresses.street);
