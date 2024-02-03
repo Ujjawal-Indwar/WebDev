639d66e21018e995789d8578b0f14a56c0ed79be
unknown <ujjawal217@gmail.com>
2024-02-03 23:57:03 +0530

    code

--- Day10/script.js
+++ Day10/script.js
@@ -1,1 +1,43 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
 
+jokeBtn.addEventListener('click',generateJoke)
+
+generateJoke()
+
+/*Using .then method 
+function generateJoke() {
+
+    const config = {
+        headers: {
+            'Accept':'application/json',
+        },
+    }
+
+
+
+
+    fetch('https://icanhazdadjoke.com', config)
+        .then((res) => res.json())
+        .then((data) => {
+            jokeEl.innerHTML = data.joke
+        })
+}
+*/
+
+
+//Using async await
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+    const res = await fetch("https://icanhazdadjoke.com", config)
+    
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
+    
+}
